5e8248123a881fabc1a590558fceeece738b1c85
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-08-24 16:49:27 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <script>
 import navbar from './components/navbar.vue';
 import AppHome from './pages/AppHome.vue';
+import AppFooter from './components/AppFooter.vue'
 export default {
     components: {
         navbar,
         AppHome,
+        AppFooter,
     }
 }
-
-
-
 </script>
 
 <template>
     <navbar />
     <AppHome />
+    <AppFooter />
 </template>
 
 <style scoped></style>
