b4b20729ba2e76f4534ad5fe3ca7ebf0473c88b0
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-08-24 10:23:44 +0200

    Initial commit [0]

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <h1>
+        Hello world!
+    </h1>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+</style>
---
+++ src/router.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// Importiamo i componenti che definiscono le varie pagine
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+
+  ],
+});
+
+export { router };
